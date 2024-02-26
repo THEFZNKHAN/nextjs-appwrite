3a657155ca227bcbd40649c52b9e67e94bc3a331
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-02-27 00:46:54 +0530

    initial Setup and create authContext and useAuth

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# nextjs-appwrite
+## NextJS-Appwrite
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <h1 className="text-3xl font-bold">This is a Next App Home Page</h1>
+        </div>
+    );
+}
