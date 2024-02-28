61e8fb36dddb1451e07e7d1d93a4e9d78be8dc7c
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-02-28 19:41:58 +0530

    create all components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="h-screen flex items-center justify-center">
-            <h1 className="text-3xl font-bold">This is a Next App Home Page</h1>
+            <h1 className="text-4xl font-bold">This is a Next App Home Page</h1>
         </div>
     );
 }
